87f41e57783bfbae1857cf6a9149c4ceaced977f
Elias Hidalgo <EliasHidalgoCriminalJustice@outlook.com>
2022-08-10 03:29:49 -0400

    g:

---
+++ syntax.js
@@ -0,0 +1,301 @@
+// This is a comment
+
+// let myName = "Elias"
+// let myInstructor = "Hidalgo"
+
+// let theColorOfAnApple = "yellow"
+// console.log("I have an apple and its color is ", theColorOfAnApple)
+
+// console.log(myName)
+// console.log(myInstructor)
+
+
+
+// let name = "Junon"
+// let hisAge = 18
+
+// let junonsSister = "Luna"
+// let isAstudent = true
+
+// console.log("Whats Junons Sisters Name" ,junonsSister)
+
+
+// let theNumberTwo = 4
+// Declaring Variable
+// console.log(theNumberTwo)
+
+// theNumberTwo = 2
+// Reassigning the value of the old variable
+// console.log(theNumberTwo)
+
+
+
+// let myBankAccount = 5000
+
+// withdrawl that happened
+
+// deposit that happened
+
+// myBankAccount = 79
+
+
+// let eliasQuestion = 10 * 10 
+
+// One way to do it:
+//console.log("Answer : ",eliasQuestion)
+
+// Second way to do it:
+// console.log(eliasQuestion)
+
+
+
+// Below showcases when an error appears:
+
+// const theNumberTwo = 4
+
+// console.log(theNumberTwo)
+
+
+// Placing the theNumberTwo = 2 below will promote an error
+// A TypeError will showcase. The assignment that we're trying to give 
+// this, which would be the = <--- the equal sign; you attempted to
+// assign a new value to something thats constant.
+// it already has a value
+// theNumberTwo = 2
+// console.log(theNumberTwo)
+// console.log(theNumberTwo)
+// console.log(theNumberTwo)
+// console.log(theNumberTwo)
+
+
+// No errors will showcase, when approaching it like this:
+// <--- if you type this; these are just placeholders to take notes.
+
+// let theNumberTwo = 4
+
+// console.log(theNumberTwo)
+
+// theNumberTwo = 2
+// console.log(theNumberTwo)
+// console.log(theNumberTwo)
+// console.log(theNumberTwo)
+// console.log(theNumberTwo)
+
+
+// notes:
+// let myFathersGender = "male"
+
+
+
+
+// Important information to be aware about:
+
+
+// let userLoggedIn = false 
+
+// let eliasQuestion = 10 + 10
+
+// userLoggedIn = true 
+// console.log(eliasQuestion)
+
+// Nobody knows what SheldonsSpot Is:
+// If I console log that
+// A message will pop up saying: SheldonsSpot is not defined
+// This is different than a Undefined Term 
+// However; this is all what we need to know about Undefined right now
+// Until we get into the example
+// And we're going to run into Undefined Values, and things regarding
+// that nature, when we chase our errors, when we're doing testing
+// and things like that
+// The most important thing to know would be, for example: 
+// "Why would we use Undefined", if its not define, why would I use it
+// Well because if I had a form, and needed my users to enter
+// their name, their age and phone number but under the condition
+// that none of those values can be undefined or new or null
+// So what this does is, setting yourself up for success, by 
+// Saying, there is only specific number of data types that are
+// accessible for my form.
+// Programmers created undefined, null and nan to represent when 
+// values are not there or lets say
+// if you had a form and you were expecting back a number 
+// and somebody put an asterisk in it, they may say
+// not sure what that is, thats undefined
+// or thats a null value
+// that doesn't equate to anything
+// you can't multiply 6 times asterisk
+// they may say "what does that mean"
+// console.log(SheldonsSpot)
+
+
+
+
+// If I wanted to use the Assignment Operator
+// let perscholas = "Tech Company" 
+
+// A Arithmetic Operator would be like:
+// 2 + 1 - 7 / 2
+
+// "Comparsion Operators" If I wanted to +, -, *, / or ^
+// So if I wanted to do:
+
+// let five = 10 / 2 <--- Arithmetic
+
+
+
+// 5 != 9
+
+// console.log(5 && 4)
+// the number 4 will appear
+
+// but if i did it this way:
+
+
+// 5 will appear as the answer:
+// 5 != 9 
+// console.log(5 | 4)
+
+
+// this one only checks the value
+// this would come out to equaling as true
+// 5 == "5"
+// --> this data type is a number --> 5 ==  this data type is a string ->"5" 
+// thats why they're different
+// Loose -> will say its true because it only checks value
+
+// where as, the strict operator, that checks the value and the data type
+// 5 === 5
+// if we were to run this with a triple === <-- equal with a Strict
+// it will give us false, because even though the value is the same
+// the Data Types are not
+// One is a number and one is a String
+
+// command below comes out to be "true"
+// console.log( 5 === 5)
+// the command in the bottom comes out to be "false"
+// console.log(5 === "5")
+
+// both will come out to be true because its under the 5.0
+// let eliasAge = 5.0
+// console.log( 5 === 5)
+// console.log(5 === eliasAge)
+
+// and it will come out --> True because 5 is not 7.0
+// let eliasAge = 7.0
+// console.log( 5 === 5)
+// console.log(5 !== eliasAge)
+
+
+
+// Exercise 1:
+// declare two variables:
+let theNumberFive = 10
+// assign them numerical values:
+console.log(theNumberFive)
+// declare a third variable and assign it the sum of above two variables:
+theNumberTwo = 5
+
+
+// Exercise 2:
+// Include a few more variables:
+console.log (2 + 1 - 7 / 2)
+// switch up your operators:
+console.log (2 - 1 * 7 + 2)
+
+
+
+// Exercise 3:
+// Declare and assign values to 10 different variables:
+console.log (33 - 12 * 57 + 95)
+console.log (77 + 4 - 8 / 22)
+console.log (22 - 5 - 55 * 100)
+console.log (55 * 42 - 25 * 57)
+console.log (7 + 11 - 5 * 9)
+console.log (4 / 3 * 8 * 11)
+console.log (59 + 31 - 21 * 37)
+console.log (111 * 9 - 3 / 19)
+console.log (77 / 51 - 7 * 32)
+console.log (8 + 42 - 51 * 89)
+
+// Find average of first 5 variables
+-556
+80.63636363636364
+-5483
+885
+-27
+
+// Find average of last 5 variable
+117.33333333333333333
+-687
+998.84210526315789474
+-222.49019607843137255
+-4489
+
+// Find average of above two results
+-556
+80.63636363636364
+
+
+// Exercise 4:
+// Find the average of ( 4, 10, 22, -30, 55);
+console.log (4 + 10 + 22 + 30 + 55)
+console.log (121 / 5)
+// Average found = 24.2
+
+// Find the average of ( 68, 7, -22, 9, 100);
+console.log (162 / 5)
+// Average found = 32.4
+
+// Find the average of the above;
+// Find the average of ( 4, 10, 22, -30, 55);
+
+Average: 12.2
+
+Average = 	
+Sum
+Count
+= 	
+61
+5
+= 	12.2
+
+Sum	61
+Count	5
+Median	10
+Geometric Mean	NAN
+Largest	55
+Smallest	-30
+Range	85
+
+
+// Find the average of ( 68, 7, -22, 9, 100);
+Average: 32.4
+
+Average = 	Sum
+
+Count
+
+= 	162
+
+5
+
+= 	32.4
+
+Sum	162
+Count	5
+Median	9
+Geometric Mean	NAN
+Largest	100
+Smallest	-22
+Range	122
+
+
+
+
+
+
+
+
+
+
+
+
